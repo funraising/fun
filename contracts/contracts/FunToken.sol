f18ffd8f3200b0c23fbb44e70ab255640221134a
Juraj Piar <juraj.piar@icloud.com>
2024-06-01 12:05:12 +0200

    feat: adds fun token logic

--- contracts/contracts/FunToken.sol
+++ contracts/contracts/FunToken.sol
@@ -2,19 +2,46 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 
 /// Tokens that are bought in the funraising campains.
-contract FunToken is ERC20 {
-    // solhint-disable-next-line no-empty-blocks
-    constructor() ERC20("FunToken", "FUN") {} //TODO: Have the naming configurable based on input
+contract FunToken is ERC20, Ownable {
+    bool private _locked;
+    uint256 private _maxSupply;
+
+    constructor(string memory name, string memory symbol, uint256 maxSupply) ERC20(name, symbol) Ownable() {
+        _locked = true;
+        _maxSupply = maxSupply;
+    }
 
     function transfer(address to, uint256 amount) public virtual override returns (bool) {
-        // TODO: If campain is unsuscesfull than transfer can be only back to the bonding contract
-        //  If succesfull then any transfer
-        // If still in progress no transfer allowed
+        require(!_locked, "FunToken: token is locked");
 
-        address owner = _msgSender();
-        _transfer(owner, to, amount);
+        _transfer(_msgSender(), to, amount);
+
         return true;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner returns (bool){
+        require(totalSupply() + amount <= _maxSupply, "FunToken: max supply exceeded");
+
+        _mint(to, amount);
+
+        return true;
+    }
+
+    function burn(uint256 amount) public returns (bool) {
+        _burn(_msgSender(), amount);
+
+        return true;
+    }
+
+    function lock() public onlyOwner {
+        _locked = true;
+    }
+
+    function unlock() public onlyOwner {
+        _locked = false;
     }
 }
--- contracts/deploy/fun_token.js
+++ contracts/deploy/fun_token.js
@@ -1,7 +1,16 @@
 
 module.exports = async ({ deployments, getNamedAccounts, getUnnamedAccounts, network }) => {
   const { deployer } = await getNamedAccounts()
-  const tokenDeployment = await deployments.deploy("FunToken", { from: deployer })
+
+  const name = "FunToken";
+  const symbol = "FUN";
+  const initialSupply = 1000000;
+
+  const tokenDeployment = await deployments.deploy("FunToken", {
+    from: deployer,
+    args: [name, symbol, initialSupply],
+    log: true,
+});
   const token = await hre.ethers.getContractAt("FunToken", tokenDeployment.address)
 }
 
