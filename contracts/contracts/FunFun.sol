72883e89461b52307fc93da037e7d14276f49350
Juraj Piar <juraj.piar@icloud.com>
2024-06-01 11:05:37 +0200

    feat: use node20

---
+++ contracts/contracts/FunFun.sol
@@ -0,0 +1,4 @@
+
+// The represetation of the campain as a bonding curve-based token
+contract FunFun {
+}
--- contracts/contracts/FunToken.sol
+++ contracts/contracts/FunToken.sol
@@ -2,14 +2,18 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 
 /// Tokens that are bought in the funraising campains.
-contract BondingToken is ERC20 {
+contract FunToken is ERC20, Ownable { // FIXME: probably not ERC20
+    enum CampainState {InProgress, Success, Fail};
+    
     // solhint-disable-next-line no-empty-blocks
     constructor() ERC20("FunToken", "FUN") {} //TODO: Have the naming configurable based on input
 
     function transfer(address to, uint256 amount) public virtual override returns (bool) {
-        // TODO: If campain is unsuscesfull than transfer can be only back to the bonding contract
+        // TODO: If campain is unsuscesfull then transfer can only be sent back to the bonding contract
         //  If succesfull then any transfer
         // If still in progress no transfer allowed
 
