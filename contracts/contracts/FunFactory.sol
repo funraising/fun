7954e756ac6618ed9e8f50aaa700b809e47351eb
Juraj Piar <juraj.piar@icloud.com>
2024-06-01 12:32:28 +0200

    feat(factory): adds fun mapping

--- contracts/contracts/FunFactory.sol
+++ contracts/contracts/FunFactory.sol
@@ -1,17 +1,26 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./FunFun.sol";
 
 contract FunFactory {
-    constructor(
-        IERC20 fundingToken,
-        IERC20 funToken
-    ) {
-        // TODO: Save things
+    mapping(address => FunFun[]) private _funByFunder;
+
+    function makeFun(
+    ) public returns (FunFun) {
+        FunFun fun = FunFun(address(0)); // TODO: Deploy FunFun and save the address
+        _funByFunder[msg.sender].push(fun); 
+
+        return fun;
     }
 
     // TODO: Deploys bonding contract
     // TODO: Deploys FunToken with the parameters
     // TODO: Last transaction setup Uniswap liquid pool
+
+
+    function getFunByFunder(address funder) public view returns (FunFun[] memory) {
+        return _funByFunder[funder];
+    }
 }
