3490dc39db7772b6b0e919e5d8aa77f18711aedd
Juraj Piar <juraj.piar@icloud.com>
2024-06-01 13:40:27 +0200

    feat(factory): makes fun and token

--- contracts/deploy/fun_factory.js
+++ contracts/deploy/fun_factory.js
@@ -1,16 +1,7 @@
 module.exports = async (environment) => {
   const testToken = await deployments.get("TestToken")
   const funToken = await deployments.get("FunToken")
-
-  const args = [
-    "name",
-    "symbol",
-    "imageURI",
-    "raisinToken",
-    Date.now(),
-    1000000,
-    6000000
-  ];
+  const args = [testToken.address, funToken.address]
   const { deployer: from } = await getNamedAccounts()
   const factory = await deployments.deploy("FunFactory", { args, from })
   console.log("Deployed FunFactory at:")
